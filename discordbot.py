7328a8b08fd06b03ca046086b436f4802210cdaf
Minokich <40020414+minokich@users.noreply.github.com>
2025-01-15 23:35:40 +0900

    feat: add error handling

--- discordbot.py
+++ discordbot.py
@@ -2,9 +2,10 @@ import discord
 import io
 import analysImage
 from PIL import Image
+import os
 
 # ボットのトークン
-TOKEN = ""
+TOKEN = os.environ['TOKEN']
 
 # Discord Intentsの設定
 intents = discord.Intents.default()
@@ -24,7 +25,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     # 特定のチャンネルID
-    target_channel_id = 0
+    target_channel_id = int(os.environ['CHANNEL_ID'])
 
     # 投稿されたメッセージが特定のチャンネルからのものかチェック
     if message.channel.id == target_channel_id:
@@ -32,32 +33,44 @@ async def on_message(message):
             # 画像が添付されているかチェック
             if len(message.attachments) > 0:
                 total_images = len(message.attachments)
-                for index, attachment in enumerate(message.attachments, start=1):
+                for index, attachment in enumerate(message.attachments,
+                                                   start=1):
                     if attachment.content_type.startswith("image"):
                         # 画像のダウンロード
                         image_data = await attachment.read()
                         image_binary = io.BytesIO(image_data)
                         image = Image.open(image_binary)
 
-                        # 画像解析 
-                        # TODO: なんかいいかんじに解析を呼び出す
-                        # img_1 = Image.open("./images/MuMu12-20250115-155119.png").convert('RGB')
-                        result1, result2, result3 = analysImage.main(image)
-                        scores = {"vo": result1, "da": result2, "vi": result3}
+                        # 画像解析
+                        try:
+                            result1, result2, result3, score = analysImage.main(
+                            image)
+                            scores = {
+                                "vo": result1,
+                                "da": result2,
+                                "vi": result3,
+                                "total": score
+                            }
+
+                        except:
+                            scores = {}
 
                         # 画像解析から取得したスコアを返信
                         await reply(message, scores)
-                        
+
 
 async def reply(message, scores):
     if scores != {}:
         print(scores)
-        upload_message = f"Vo: {scores["vo"]}, Da: {scores["da"]}, Vi: {scores["vi"]}"
+        vo = scores["vo"]
+        da = scores["da"]
+        vi = scores["vi"]
+        upload_message = f"Vo: {vo}, Da: {da}, Vi: {vi}"
         await message.reply(upload_message)
-    else: 
+    else:
         await message.reply("解析に失敗しました")
 
 
 if __name__ == "__main__":
-    # ボットを起動
     client.run(TOKEN)
+
